44616edf2d57eec478a6601405317203f906be4c
Magesh <mageshkumar2891@gmail.com>
2024-01-09 20:14:14 +0530

    classComponent modified

---
+++ src/components/classComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class ClassComponent extends React.Component{
+    // constructor(){
+    //     super()
+    // }
+    render(){
+        return(
+        <div>
+            <h1>New Project</h1></div>
+        );
+    }
+}
+
+export default ClassComponent
